5e3573bfcb7b2a37187da172d4a2893ab6ed2514
Mohit1x <mohitvyas746@gmail.com>
2024-08-18 20:23:35 +0530

    Added Hero Section template by Mohit Vyas

---
+++ Hero-Section-Components/MohitVyasHeroSection/src/App.jsx
@@ -0,0 +1,15 @@
+// import bg_img from "./assets/bg_img.png";
+
+import Hero from "./components/Hero";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="h-full flex flex-col">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
